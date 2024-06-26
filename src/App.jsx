e2281c13912404cab14bb03544eeff28172f3138
Mohd Afzal <afzalnaved0000@gmail.com>
2024-06-26 16:32:32 +0530

    shadcn configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
    <div className="bg-black text-white">
     Hello ai
+    <Button>Hello</Button>
    </div>
   )
 }
